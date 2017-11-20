48e482e5123f45753ca5275cca3201358dac5e99
vladimir <vladimirsvsv77@gmail.com>
2017-11-20 23:53:32 +0300

    added asr

--- talking_robot.py
+++ talking_robot.py
@@ -1,18 +1,12 @@
+
 # -*- coding: utf-8 -*-
 import requests
 
 from examples.Python import snowboydecoder, settings
 from yandex_speech import TTS
 import os
-import re
-import sys
 
-from google.cloud import speech
-from google.cloud.speech import enums
-from google.cloud.speech import types
-import pyaudio
-from six.moves import queue
-
+from examples.Python.talking_robot.google_asr import start_google_asr
 
 RATE = 16000
 CHUNK = int(RATE / 10)  # 100ms
@@ -32,110 +26,44 @@ def get_answer_bing(question):
     req = requests.get(bing_endpoint+question, headers={'Ocp-Apim-Subscription-Key': settings.bing_api_key})
     return  req.json()['webPages']['value'][0]['snippet']
 
-
-class MicrophoneStream(object):
-    def __init__(self, rate, chunk):
-        self._rate = rate
-        self._chunk = chunk
-
-        self._buff = queue.Queue()
-        self.closed = True
-
-    def __enter__(self):
-        self._audio_interface = pyaudio.PyAudio()
-        self._audio_stream = self._audio_interface.open(
-            format=pyaudio.paInt16,
-            channels=1, rate=self._rate,
-            input=True, frames_per_buffer=self._chunk,
-            stream_callback=self._fill_buffer,
-        )
-
-        self.closed = False
-
-        return self
-
-    def __exit__(self, type, value, traceback):
-        self._audio_stream.stop_stream()
-        self._audio_stream.close()
-        self.closed = True
-        self._buff.put(None)
-        self._audio_interface.terminate()
-
-    def _fill_buffer(self, in_data, frame_count, time_info, status_flags):
-        self._buff.put(in_data)
-        return None, pyaudio.paContinue
-
-    def generator(self):
-        while not self.closed:
-            chunk = self._buff.get()
-            if chunk is None:
-                return
-            data = [chunk]
-
-            while True:
-                try:
-                    chunk = self._buff.get(block=False)
-                    if chunk is None:
-                        return
-                    data.append(chunk)
-                except queue.Empty:
-                    break
-
-            yield b''.join(data)
-
-
-def listen_print_loop(responses):
-    num_chars_printed = 0
-    for response in responses:
-        if not response.results:
-            continue
-
-        result = response.results[0]
-        if not result.alternatives:
-            continue
-
-        transcript = result.alternatives[0].transcript
-
-        overwrite_chars = ' ' * (num_chars_printed - len(transcript))
-
-        if not result.is_final:
-            sys.stdout.write(transcript + overwrite_chars + '\r')
-            sys.stdout.flush()
-
-            num_chars_printed = len(transcript)
-
-        else:
-            print(transcript + overwrite_chars)
-            say_vera(get_answer_bing(transcript + overwrite_chars))
-            detector.start(detected_callback)
-            break
-
-
-def start_google_asr():
-    language_code = 'ru'  # a BCP-47 language tag
-
-    client = speech.SpeechClient()
-    config = types.RecognitionConfig(
-        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=RATE,
-        language_code=language_code)
-    streaming_config = types.StreamingRecognitionConfig(
-        config=config,
-        interim_results=True)
-
-    with MicrophoneStream(RATE, CHUNK) as stream:
-        audio_generator = stream.generator()
-        requests = (types.StreamingRecognizeRequest(audio_content=content)
-                    for content in audio_generator)
-
-        responses = client.streaming_recognize(streaming_config, requests)
-
-        listen_print_loop(responses)
-
-
 def detected_callback():
-    start_google_asr()
+    asr = start_google_asr()
+    print(asr)
+    detector = snowboydecoder.HotwordDetector("hello_vera.pmdl", sensitivity=0.5, audio_gain=1)
+    detector.start(detected_callback)
 
 
 detector = snowboydecoder.HotwordDetector("hello_vera.pmdl", sensitivity=0.5, audio_gain=1)
 detector.start(detected_callback)
+
+
+# def get_bloomberg_titles():
+#     from bs4 import BeautifulSoup
+#
+#     news_url = 'https://www.bloomberg.com/view/topics/russia'
+#
+#     req = requests.get(news_url)
+#
+#     soup = BeautifulSoup(req.text, "html.parser")
+#     titles = soup.find_all('h1', {'class': 'title_3Ob7U'})
+#
+#     titles_text = []
+#
+#     for i in titles:
+#         titles_text.append(i.text)
+#
+#     return titles_text[:3]
+#
+#
+# news = requests.post('https://ai.robotvera.com/stafory/getbloombergtitles/')
+#
+# print(news.text)
+# news_speak = 'Top news: '+news[0].replace('\n', '')+'. Also in Russia: '+news[1].replace('\n','')
+#
+#
+# json_responce = {"outputSpeech": {
+#     "type": "SSML",
+#     "ssml": "<speak>"+news_speak+"</speak>"
+# }}
+#
+# print(json_responce)
